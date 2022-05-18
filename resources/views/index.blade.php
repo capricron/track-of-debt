08cf704341592b71a5a8b673c15eb041ad824fa6
shalexandeer <shalexandeer@student.telkomuniversity.ac.id>
2022-05-18 20:31:59 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
    
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
-
+    
     <title>Track of Debts</title>
   </head>
   <body>
